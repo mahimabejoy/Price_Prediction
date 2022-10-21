ff3746f646b3aa0935eaa8073b32342c7aa142d1
mahimabejoy <mahimabejoy@gmail.com>
2022-10-21 11:52:00 +0530

    webapp created

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# housepriceprediction
+# Housepriceprediction
+### An end-to-end project on predicting house price 
+
+
+
+### Software And Tools Requirements
+
+1. HerokuAccount 
+2. VSCodeIDE
+3. GitCLI
---
+++ app.py
@@ -0,0 +1,34 @@
+import json
+import pickle
+from flask import Flask,request,app,jsonify,url_for,render_template
+import numpy as np
+import pandas as pd
+
+app=Flask(__name__)
+regmodel=pickle.load(open('regmodel.pkl','rb'))
+scaler=pickle.load(open('scaler.pkl','rb'))
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/predict_api',methods=['POST'])
+def predict_api():
+    data=request.json['data']
+    print(data)
+    print(np.array(list(data.values())).reshape(1,-1))
+    new_data=scaler.transform(np.array(list(data.values())).reshape(1,-1))
+    output=regmodel.predict(new_data)
+    print(output[0])
+    return jsonify(output[0])
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    data=[float(x) for x in request.form.values()]
+    final_input=scaler.transform(np.array(data).reshape(1,-1))
+    print(final_input)
+    output=regmodel.predict(final_input)[0]
+    return render_template('home.html',prediction_text="House price is {}".format(output))
+
+if __name__==" main ":
+    app.run(debug=True)
